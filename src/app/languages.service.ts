b952ac2f6209786c70bff70466aa7397431b3294
dianjuar <dianjuar@gmail.com>
2017-03-28 03:08:36 -0400

    Improvements on the language service.
    Created regular expression to verify if a URL is valid and if the PTC introduced has a supported language.

--- src/app/languages.service.ts
+++ src/app/languages.service.ts
@@ -9,32 +9,85 @@ export class LanguagesService {
 	/**
 	 * List of the supported languages
 	 */
-	private languages: Array<Language>;
+	private _languages: Array<Language>;
+
+	/**
+	 * Regular expression of a valid URL
+	 */
+	private _regexURL;
+
+	/**
+	 * Regular expression to validate if the language's PTC is supported
+	 */
+	private _regexPTCLanguage;
 
 	constructor() {
-
 		// init the vector
-		this.languages = new Array<Language>();
+		this._languages = new Array<Language>();
 
+		// init regular expreson of valid URL
+		this._regexURL = /((https?)(:\/\/))?(translate\.wordpress\.org\/projects\/)(wp-?(themes|plugins)?\/)(.*)(default\/?)/gi;
+
+		// init _languages
 		this._loadLanguages();
 
-		console.log( this.languages );
-	}
+		// init regular expresion of valid language's PTC
+		this._regexPTCLanguage = new RegExp('(.*)'+this._getRegExLanguages()+'(\/default)', 'gi');
 
-	getLanguages(){
-		return this.languages;
+		console.log(this._regexPTCLanguage);
 	}
 
 	/**
-	 * Load all languages
+	 * _languages getter
+	 */
+	getLanguages() {
+		return this._languages;
+	}
+
+	/**
+	 * Given an URL verify if it structure is a valud URL
+	 * @param URL url to verify if it is valid
+	 */
+	isURL_ValidRegEx(URL: string): boolean {
+		return URL.match(this._regexURL) != null;
+	}
+
+	/**
+	 * Validate if the language's PTC is supported
+	 * @param URL url to verify if the language of the PTC in the url is supported
+	 */
+	isPTC_LanguageValid(URL: string): boolean {
+		return URL.match(this._regexPTCLanguage) != null;
+	}
+
+	/**
+	 * Regular expresion that contains all the slugs created dinamically in string format
+	 * 
+	 * @return string
+	 * @example (pt-br|pt|es|es_ve|...)
+	 */
+	_getRegExLanguages() {
+		return '(' +
+			this._languages.reduce((acum: string, current: Language, index) => {
+				let pipe = index == this._languages.length - 1 ? '' : '|';
+				return acum + current.slug.replace('_','-') + pipe;
+			}, '')
+			+ ')';
+	}
+
+	/**
+	 * Load all languages.
+	 * Read from a JSON and put it on the variable _languages
 	 */
 	private _loadLanguages() {
 
-		this.languages = languagesJSON.map(langJSON => <Language>({ name: langJSON.name, 
-																	slug: langJSON.slug, 
-																	native: langJSON.native,
-																    related: Array<Language>()})
-										  );
+		this._languages = languagesJSON.map(langJSON => <Language>({
+			name: langJSON.name,
+			slug: langJSON.slug,
+			native: langJSON.native,
+			related: Array<Language>()
+		})
+		);
 		this._loadRelatedLanguages();
 	}
 
@@ -43,13 +96,13 @@ export class LanguagesService {
 	 * Once the languages are loaded we can fill in the related array of each language. 
 	 * This way is efficent because we use a reference of a language instead of a new element.
 	 */
-	private _loadRelatedLanguages(){
-		this.languages.map( language => {
-			
-			// Get the parent slug of the current language
-			let parentLangSlug = this._getParentLanguage( language.slug );
+	private _loadRelatedLanguages() {
+		this._languages.map(language => {
 
-			language.related = this.languages.filter( l => ~l.slug.toLowerCase().indexOf(parentLangSlug) && l.slug != language.slug );
+			// Get the parent slug of the current language
+			let parentLangSlug = this._getParentLanguage(language.slug);
+
+			language.related = this._languages.filter(l => ~l.slug.toLowerCase().indexOf(parentLangSlug) && l.slug != language.slug);
 		});
 	}
 
@@ -58,9 +111,9 @@ export class LanguagesService {
 	 * @param slug The slug of the language
 	 * @example The parent slug of the slug "es_VE" is "es"
 	 */
-	private _getParentLanguage(slug: String){
+	private _getParentLanguage(slug: String) {
 		return slug.split(/[-_]/g)[0];
 	}
 
-	
+
 }
